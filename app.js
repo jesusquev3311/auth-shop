3a182ac71b5fb537f8f48b2bfa9ddcee989fbf96
jesusquev3311 <jesusquev3311@gmail.com>
2022-06-24 19:28:58 -0400

    adding 404 middleware

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
+// setting CORS
 const corsOptions = {
     origin: "localhost:8001",
 };
 
 app.use(cors(corsOptions));
+
+// Parsing requests
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to the application." });
+});
+
+// Sending Not Found requests to 404 view
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+})
+
 
 module.exports = app;
