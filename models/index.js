d35eb2379167ec6e468fae7a4bc388625c7287bc
jesusquev3311 <jesusquev3311@gmail.com>
2022-06-24 20:05:33 -0400

    adding index model

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const CONFIG = require("../config/db.config");
+
+const Sequelize = require("sequelize");
+const products = require("./products.model");
+
+const { DB, USER, PASSWORD, HOST, dialect, pool } = CONFIG;
+
+const connection = new Sequelize(
+    DB,
+    USER,
+    PASSWORD,
+    {
+        host: HOST,
+        dialect,
+        operatorsAliases: false,
+        pool: {
+            max: pool.max,
+            min: pool.min,
+            acquire: pool.acquire,
+            idle: pool.idle,
+        }
+    },
+);
+
+const db = {
+    Sequelize,
+    connection,
+    products,
+};
+
+module.exports = db;
